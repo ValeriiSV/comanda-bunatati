a7f50806308ff6240583963c44c52016f11ca3a3
Valeriu Svetlicenco <valerkasvetlicenco@icloud.com>
2026-09-10 00:00:38 +0300

    Wrap routes in error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import OrderPage from './pages/OrderPage';
 import AdminPage from './pages/AdminPage';
 import MiaPaymentPage from './pages/MiaPaymentPage';
+import AppErrorBoundary from './components/AppErrorBoundary';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<OrderPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/admin/mia" element={<MiaPaymentPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AppErrorBoundary>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<OrderPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/admin/mia" element={<MiaPaymentPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AppErrorBoundary>
   </StrictMode>,
 );
