58c4934822edd761def71ce734cbf8c48205581f
Valeriu Svetlicenco <valerkasvetlicenco@icloud.com>
2026-09-09 23:15:48 +0300

    Add MIA settings route

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import OrderPage from './pages/OrderPage';
 import AdminPage from './pages/AdminPage';
+import MiaPaymentPage from './pages/MiaPaymentPage';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<OrderPage />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin/mia" element={<MiaPaymentPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
