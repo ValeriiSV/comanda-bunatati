e7a22bff189ca4c0ce59fc5a1aa9d89143af6d1a
Valeriu Svetlicenco <valerkasvetlicenco@icloud.com>
2026-09-10 21:51:56 +0300

    Add order status route

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import OrderPage from './pages/OrderPage';
 import AdminPage from './pages/AdminPage';
 import MiaPaymentPage from './pages/MiaPaymentPage';
+import OrderStatusPage from './pages/OrderStatusPage';
 import AppErrorBoundary from './components/AppErrorBoundary';
 import './index.css';
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<OrderPage />} />
+          <Route path="/status" element={<OrderStatusPage />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/admin/mia" element={<MiaPaymentPage />} />
         </Routes>
